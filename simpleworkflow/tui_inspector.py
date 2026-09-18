21a91fc0338b0870081890289ae0f47b4c164138
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-09-18 10:16:26 -0300

    style(tui): sort resource imports case-insensitively

--- simpleworkflow/tui_inspector.py
+++ simpleworkflow/tui_inspector.py
@@ -15,7 +15,7 @@ from textual.message import Message
 from textual.widgets import Button, Collapsible, DataTable, Static
 
 from .monitor import AttemptSnapshot, TaskSnapshot
-from .tui_resources import InspectableResource, discover_attempt_resources
+from .tui_resources import discover_attempt_resources, InspectableResource
 
 
 _STATUS = {
